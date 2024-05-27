079749faacd90e16cc6d32b1b6f7a413ea2117cc
Kaws <akmyat116@gmail.com>
2024-05-27 20:16:02 +0800

    first commit

---
+++ a2/script.js
@@ -0,0 +1,11 @@
+
+// Hamburger menu function
+function burgerclick() {
+	var nav = document.getElementById("mobilenavigation");
+	if (nav.style.display === "block"){
+		nav.style.display = "none";
+	} else {
+		nav.style.display = "block";
+	}
+}
+	
